1d0f563a1320db13137f2960422cf877e92d5167
Ben <rollbahn.gib-5o@icloud.com>
2025-11-15 18:26:25 +0100

    test

--- app/config/database.py
+++ app/config/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://USER:PASSWORD@HOST:PORT/DATABASE"
+DATABASE_URL = "postgresql+psycopg2://postgres:1234@db.vgpnerdevdazcckwrnze.supabase.co:5432/postgres"
+#                  ^ User   ^Passwort                    ^ Host                                   ^ Port  ^ DB-Name
 
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
