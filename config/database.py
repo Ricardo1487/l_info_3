2839160d04278bfd3afc8dc8f273e20e32f8377e
Ben <rollbahn.gib-5o@icloud.com>
2025-11-15 17:24:23 +0100

    test

--- config/database.py
+++ config/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:1234@db.vgpnerdevdazcckwrnze.supabase.co:5432/postgres"
-# rico wie geht das nochmal mit den passwörtern??
+DATABASE_URL = "postgresql+psycopg2://USER:PASSWORD@HOST:PORT/DATABASE"
+
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
