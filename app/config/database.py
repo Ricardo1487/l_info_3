1d3ae87c1c68e4e804d9728096740a3f5481b09d
Ben <rollbahn.gib-5o@icloud.com>
2025-11-22 15:41:28 +0100

    test

--- app/config/database.py
+++ app/config/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
-# Lädt die .env-Datei aus dem Projektroot
 load_dotenv()
+
 DB_USER = os.getenv("DATABASE_USER")
 DB_PASSWORD = os.getenv("DATABASE_PASSWORD")
 DB_HOST = os.getenv("DATABASE_HOST")
@@ -14,8 +14,9 @@ DB_NAME = os.getenv("DATABASE_NAME")
 DATABASE_URL = (
     f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
     f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"?sslmode=require"
 )
-#                  ^ User   ^Passwort                    ^ Host                                   ^ Port  ^ DB-Name
 
-engine = create_engine(DATABASE_URL, echo=True, future=True)
+engine = create_engine(DATABASE_URL, echo=False, future=True)
+
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
